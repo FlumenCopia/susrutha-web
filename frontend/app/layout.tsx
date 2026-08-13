48fb8c680f2039c8a79a77082ad5ab034e844ece
akhilsflumenx <akhilsflumencopia@gmail.com>
2026-08-13 12:11:08 +0530

    feat: initialize Ayora design system and implement home treatments showcase section

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
