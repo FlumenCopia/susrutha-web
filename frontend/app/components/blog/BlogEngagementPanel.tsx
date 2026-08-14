01ea6c6aad80f1097dba66fc14beb79f97bdc03a
akhilsflumenx <akhilsflumencopia@gmail.com>
2026-08-15 01:52:41 +0530

    feat: add BlogEngagementPanel component to display article engagement sections

--- frontend/app/components/blog/BlogEngagementPanel.tsx
+++ frontend/app/components/blog/BlogEngagementPanel.tsx
@@ -1,13 +1,13 @@
 import { AuthorSection } from "./AuthorSection";
 import { FooterCta } from "./FooterCta";
-import { NewsletterCta } from "./NewsletterCta";
+// import { NewsletterCta } from "./NewsletterCta";
 import { TrendingArticles } from "./TrendingArticles";
 
 export function BlogEngagementPanel() {
   return (
     <section className="blog-premium-engagement" aria-label="Blog engagement">
       <TrendingArticles />
-      <NewsletterCta />
+      {/* <NewsletterCta /> */}
       <AuthorSection />
       <FooterCta />
     </section>
